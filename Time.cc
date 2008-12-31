e1913db80bf1b7c0fdaa990279a9a8799cc136e3
mmuller <mmuller@localhost>
2008-12-31 13:29:36 -0500

    [svn] Added TimeDelta and Time classes.

---
+++ Time.cc
@@ -0,0 +1,32 @@
+/*===========================================================================*\
+     
+    Copyright (C) 2008 Michael A. Muller
+ 
+    This file is part of spug++.
+ 
+    spug++ is free software: you can redistribute it and/or modify it under the 
+    terms of the GNU Lesser General Public License as published by the Free 
+    Software Foundation, either version 3 of the License, or (at your option) 
+    any later version.
+ 
+    spug++ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+ 
+    You should have received a copy of the GNU Lesser General Public License 
+    along with spug++.  If not, see <http://www.gnu.org/licenses/>.
+ 
+\*===========================================================================*/
+
+#include <sys/time.h>
+#include <time.h>
+#include "Time.h"
+
+using namespace spug;
+
+Time Time::now() {
+    timeval tv;
+    gettimeofday(&tv, 0);
+    return Time(tv.tv_sec, tv.tv_usec);
+}
